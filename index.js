ab3743403e033f2659a02e6e98087f8baff9e67e
刘伟坤 <1834183435@qq.com>
2025-09-26 15:37:06 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const url = require('url');
+const querystring = require('querystring');
+const main = require("./main");
+
+const server = http.createServer(async (req, res) => {
+  res.setHeader("content-Type", "text/plain;charset=utf-8");
+  if (req.url.startsWith('/sport')) {
+    const parsedUrl = url.parse(req.url);
+    const query = querystring.parse(parsedUrl.query);
+    const result = await main.sport(query)
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(result);
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain', });
+    // 发送错误消息并结束响应
+    res.end('Method Not Allowed\n');
+  }
+});
+
+server.listen(6789, () => {
+  console.log("服务器开启成功,请访问 http://localhost:6789");
+});
